4f193a63cf60eaccff89aafb30bcf955501426c7
Clay Richard <alanclayrichard@gmail.com>
2025-11-20 13:31:55 -0600

    Clean up __init__.py by removing unused imports
    Remove imports and __all__ declaration from __init__.py

--- src/modelomics/__init__.py
+++ src/modelomics/__init__.py
@@ -1,9 +1,1 @@
-from .protein_graph import BaseProteinGraphBuilder
-from .atom_graph import AtomGraphBuilder
-from .residue_graph import ResidueGraphBuilder
 
-__all__ = [
-    "BaseProteinGraphBuilder",
-    "AtomGraphBuilder",
-    "ResidueGraphBuilder",
-]
